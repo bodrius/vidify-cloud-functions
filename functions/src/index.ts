f1dbc12cb3433fdd337e06e37157f24feab79b08
bodrius <bogdanpechenyk@gmail.com>
2022-03-25 13:09:47 +0200

    feat(type): added type notify

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,38 @@
 import * as functions from "firebase-functions";
 
+const admin = require("firebase-admin");
+
 const { initializeApp } = require("firebase-admin/app");
 
 initializeApp();
 
-exports.sendRemoteNotification = functions.https.onRequest(
-  (request, response) => {
-    response.send(request.body);
-  }
-);
+// {
+//   "to": "<token>",
+//   "time_to_live": 86400,
+//   "collapse_key": "new_message",
+//   "delay_while_idle": false,
+//   "notification": {
+//     "title": "title",
+//     "body": "this is a noisy test",
+//     "tag": "new_message",
+//     "icon": "new_message",
+//     "color": "#18d821",
+//     "sound": "default"
+//   }
+// }
+
+exports.sendRemoteNotification = functions.https.onCall((request) => {
+  let payload = {
+    data: {
+      type: "new_chat_message",
+    },
+    notification: {
+      title: request.postTitle,
+      body: request.textMessage,
+      sound: "default",
+      badge: "1",
+    },
+  };
+
+  return admin.messaging().sendToDevice(request.token, payload);
+});
