5dcdc6748900bf2279f73f226eb628744dbe5554
bodrius <bogdanpechenyk@gmail.com>
2022-04-06 13:09:38 +0300

    feat(push_func): create two new func

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,25 +6,42 @@ const { initializeApp } = require("firebase-admin/app");
 
 initializeApp();
 
-// {
-//   "to": "<token>",
-//   "time_to_live": 86400,
-//   "collapse_key": "new_message",
-//   "delay_while_idle": false,
-//   "notification": {
-//     "title": "title",
-//     "body": "this is a noisy test",
-//     "tag": "new_message",
-//     "icon": "new_message",
-//     "color": "#18d821",
-//     "sound": "default"
-//   }
-// }
-
-exports.sendRemoteNotification = functions.https.onCall((request) => {
+exports.sendNotificationNewChatMessage = functions.https.onCall((request) => {
   let payload = {
     data: {
       type: "new_chat_message",
+    },
+    notification: {
+      title: request.postTitle,
+      body: request.textMessage,
+      sound: "default",
+      badge: "1",
+    },
+  };
+
+  return admin.messaging().sendToDevice(request.token, payload);
+});
+
+exports.sendNotificationNewOffer = functions.https.onCall((request) => {
+  let payload = {
+    data: {
+      type: "new_offer",
+    },
+    notification: {
+      title: request.postTitle,
+      body: request.textMessage,
+      sound: "default",
+      badge: "1",
+    },
+  };
+
+  return admin.messaging().sendToDevice(request.token, payload);
+});
+
+exports.sendNotificationReviewOffer = functions.https.onCall((request) => {
+  let payload = {
+    data: {
+      type: "review_offer",
     },
     notification: {
       title: request.postTitle,
